478e63f835a8df0255c587acfcde89600fba9355
ynot4 <98645055+ynot4@users.noreply.github.com>
2022-07-31 15:43:29 +0100

    removed offbeat chord changes

--- midi.py
+++ midi.py
@@ -7,11 +7,13 @@ import pygame
 
 def create_midi(chords, section, progression_length, chord_lengths):
     if progression_length == 4:
-        midi_data = pretty_midi.PrettyMIDI("chord_midi/drums_4_bars.mid")
+        midi_data = pretty_midi.PrettyMIDI("chord_midi/drums/4_bars.mid")
     elif progression_length == 8:
-        midi_data = pretty_midi.PrettyMIDI("chord_midi/drums_8_bars.mid")
+        midi_data = pretty_midi.PrettyMIDI("chord_midi/drums/8_bars.mid")
     else:
-        midi_data = pretty_midi.PrettyMIDI("chord_midi/drums_16_bars.mid")
+        midi_data = pretty_midi.PrettyMIDI("chord_midi/drums/16_bars.mid")
+
+    piano_only = pretty_midi.PrettyMIDI()
 
     piano_program = pretty_midi.instrument_name_to_program("Electric Piano 1")
     piano = pretty_midi.Instrument(program=piano_program)
@@ -68,55 +70,14 @@ def create_midi(chords, section, progression_length, chord_lengths):
             chord_index += length/2
         index += 1
 
-    # for n, chord in enumerate(chords):  # piano chords to midi
-    #     for note_name in chord.components_with_pitch(root_pitch=4):
-    #         note_number = pretty_midi.note_name_to_number(note_name)
-    #         if chord_lengths[n] == 4:
-    #             note = pretty_midi.Note(velocity=100, pitch=note_number, start=n * length, end=(n + 1) * length)
-    #         elif chord_lengths[n] == 2:
-    #             note = pretty_midi.Note(velocity=100, pitch=note_number, start=n * length, end=(n + 1) * length)
-    #         else:
-    #             raise ValueError("Length of chord is not 2 or 4 beats.")
-    #         piano.notes.append(note)
-
-    # for n, chord in enumerate(chords):  # create bouncy bass-line
-    #     m = n * length
-    #     l = length/8
-    #     for i in range(8):
-    #         if i % 2:  # skip odd values of i
-    #             continue
-    #         note_number = pretty_midi.note_name_to_number(chord.root + "1")
-    #         note = pretty_midi.Note(velocity=100, pitch=note_number, start=i * l + m, end=(i + 1) * l + m)
-    #         bass.notes.append(note)
-    #         i += 1
-    #         note_number = pretty_midi.note_name_to_number(chord.root + "2")
-    #         note = pretty_midi.Note(velocity=90, pitch=note_number, start=i * l + m, end=(i + 1) * l + m)
-    #         bass.notes.append(note)
-
     midi_data.instruments.append(piano)
     midi_data.instruments.append(bass)
-
-    # if section == "intro":
-    #     num = 0
-    # elif section == "verse":
-    #     num = 1
-    # elif section == "prechorus":
-    #     num = 2
-    # elif section == "chorus":
-    #     num = 3
-    # elif section == "postchorus":
-    #     num = 4
-    # elif section == "bridge":
-    #     num = 5
-    # elif section == "interlude":
-    #     num = 6
-    # elif section == "outro":
-    #     num = 7
-    # else:
-    #     num = 8
+    piano_only.instruments.append(piano)
 
     file_name = f"chord_midi/{section}.mid"
+    piano_file_name = f"chord_midi/piano/{section}.mid"
     midi_data.write(file_name)
+    piano_only.write(piano_file_name)
 
 
 def play_midi(midi_filename):
